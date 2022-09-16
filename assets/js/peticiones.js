722901a687b788d675db13e7f029f8de456e7d44
Daniel Marinado <daniel.marinado@gmail.com>
2022-09-16 01:14:22 -0300

    Se agrega localStorage

--- assets/js/peticiones.js
+++ assets/js/peticiones.js
@@ -1,30 +1,57 @@
-console.clear();
-
 // En constantes por convección uso mayúsculas y no CamelCase
 // Fuente: https://es.wikipedia.org/wiki/Convenci%C3%B3n_de_nombres_(programaci%C3%B3n)
 
 const CANT_REGISTROS = 20;
+const URI = `https://api.giphy.com/v1/gifs`;
 const API_KEY = "8f8g82LMwlCzZ6O4sQDkHcW7O7WalHAx";
-const API_TREND_URL = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=${CANT_REGISTROS}&offset=0`;         // BÚSQUEDAS TRENDING
-const API_SEARCH_URL = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=${CANT_REGISTROS}&offset=0`;           // BÚSQUEDAS PERSONALIZADAS
+const API_TREND_URL  = `${URI}/trending?api_key=${API_KEY}&limit=${CANT_REGISTROS}&offset=0`;         // BÚSQUEDAS TRENDING
+const API_SEARCH_URL = `${URI}/search?api_key=${API_KEY}&limit=${CANT_REGISTROS}&offset=0`;           // BÚSQUEDAS PERSONALIZADAS
 
 let offset = 0;
 let listadoRecientes = [];
 
-const button = document.getElementById('btnBuscar');
+const getStorage = () => {
+    const elemRecientes = localStorage.getItem('elemRecientes');
+    return JSON.parse(elemRecientes);
+}
 
-const addCard = (gif) => {
-    const contenedor = document.createElement("div");
-    contenedor.className = "card";
-    const contenido = document.createElement("p");
-    contenido.className = "tituloCard";
-    contenido.innerText = gif.title;
-    const contenedorImagen = document.createElement("img");
-    contenedorImagen.src = gif.images.original.webp;
+const setStorage = (word) => {
+    listadoRecientes = getStorage() ? getStorage() : [];
 
-    contenedor.appendChild(contenedorImagen);
-    contenedor.appendChild(contenido);
-    document.querySelector(".results").appendChild(contenedor);
+    if(listadoRecientes.length>2) listadoRecientes.shift();
+
+    listadoRecientes.push(word);
+    toastOkMsg(`Se ha añadido al localStorage: ${word}`);
+    localStorage.setItem('elemRecientes', JSON.stringify(listadoRecientes));
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    search();
+}
+
+const validateForm = () => {
+    const input = document.querySelector("#input");
+
+    if( input.value.trim() === '' ){
+        input.focus();
+        toastErrMsg(`Error, debe ingresar al menos un carácter distinto de vacio o nulo.`);
+        return false;
+    }
+}
+
+const printCard = (gif) => {
+    const wrapper = document.createElement("div");
+    wrapper.className = "card";
+    const content = document.createElement("p");
+    content.className = "tituloCard";
+    content.innerText = gif.title;
+    const wrapperImg = document.createElement("img");
+    wrapperImg.src = gif.images.original.webp;
+
+    wrapper.appendChild(wrapperImg);
+    wrapper.appendChild(content);
+    document.querySelector(".results").appendChild(wrapper);
 };
 
 const getGif = async (url) => {
@@ -34,37 +61,117 @@ const getGif = async (url) => {
 };
 
 const search = () => {
-    const palabra = document.getElementById("input").value;
-    clear();
-    loadApi(API_SEARCH_URL + `&q='${palabra}'`);
+    const input = document.querySelector("#input");
+    const word = input.value;
+
+    if( validateForm() === false ) return;
+
+    clearGifs();
+    loadApi(API_SEARCH_URL + `&q="${word}"`);
+    setStorage(word);
+    loadRecientes();
+    input.value = "";
 }
 
-const searchHistory = (palabra) => {
-    console.log(palabra);
-    loadApi(API_SEARCH_URL + `&q='${palabra}'`);
+const searchHistory = (word) => {
+    clearGifs();
+    loadApi(API_SEARCH_URL + `&q="${word}"`);
 }
 
-const clear = () => {
+const searchInfiniteScroll = () => {
+
+}
+
+const clearGifs = () => {
     document.querySelector(".results").innerHTML = "";
 }
 
 const loadApi = async (url) => {
     const gifs = await getGif(url);
-    console.log(gifs);
+
+    if(gifs.data.length===0) toastWarnMsg(`No se han encontrado resultados.`)
+
     for (let gif of gifs.data)
-        addCard(gif);
+        printCard(gif);
 };
 
 const loadRecientes = () => {
-    // Buscar en LocalStorage y add al listado
-    const recientes = document.getElementById('listaRecientes');
-    const items = recientes.getElementsByTagName("li");
-    for (var i = 0; i < items.length; ++i) {
-        let palabra = document.getElementById('input').value
-        items[i].addEventListener('click', searchHistory);
+    listadoRecientes = getStorage() ? getStorage() : [];
+    printRecents();
+}
+
+const printRecents = () => {
+    const wrapperEmptyRecientes = document.querySelector('#emptyRecientesErr');
+    const wrapperListaRec = document.querySelector('#listaRecientes');
+
+    (listadoRecientes.length === 0) 
+        ? wrapperEmptyRecientes.style.display = "block"
+        : wrapperEmptyRecientes.style.display = "none";
+
+    wrapperListaRec.innerHTML = "";
+
+    for (let element of listadoRecientes){
+        const wrapper = document.createElement("li");
+        wrapper.className = "wrapperReciente";
+        const content = document.createElement("a");
+        content.className = "linkReciente";
+        content.innerText = element;
+        content.addEventListener('click', () => searchHistory(element));
+       
+        wrapper.appendChild(content);
+        wrapperListaRec.appendChild(wrapper);
     }
 }
 
+const initApp = () => {
+    const formulario = document.querySelector('#frmSearch');
+    const button = document.querySelector('#btnBuscar');
+    formulario.addEventListener('submit', handleSubmit)
+    button.addEventListener('click', search);
+}
+
+const toastOkMsg = (word) => {
+    Toastify({
+        text: word,
+        duration: 4000,
+        close: true,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true,
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+        }
+    }).showToast();
+}
+
+const toastErrMsg = (msg) => {
+    Toastify({
+        text: msg,
+        duration: 2000,
+        close: true,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)"
+        }
+    }).showToast();
+}
+
+const toastWarnMsg = (msg) => {
+    Toastify({
+        text: msg,
+        duration: 4000,
+        close: true,
+        gravity: "top", 
+        position: "right", 
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(90deg, rgba(0,0,0,1) 0%, rgba(253,85,29,1) 48%, rgba(252,176,69,1) 100%)"
+        }
+    }).showToast();
+}
+
+initApp();
 loadApi(API_TREND_URL);
 loadRecientes();
-button.addEventListener('click', search);
